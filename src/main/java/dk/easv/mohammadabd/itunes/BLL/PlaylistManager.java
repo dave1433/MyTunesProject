c3de1dc94937d91a77604a05394e53d30a27da6e
Lucaz2010 <oliveirasantos.lucas.sm@gmail.com>
2024-12-18 18:58:46 +0100

    new branch with all classes

---
+++ src/main/java/dk/easv/mohammadabd/itunes/BLL/PlaylistManager.java
@@ -0,0 +1,44 @@
+package dk.easv.mohammadabd.itunes.BLL;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlaylistManager {
+    private Map<String, Playlist> playlists;
+
+    public PlaylistManager() {
+        this.playlists = new HashMap<>();
+    }
+    public Playlist createPlaylist(String name) {
+        Playlist playlist = new Playlist(name);
+        playlists.put(name, playlist);
+        System.out.println("Created playlist: " + name);
+        return playlist;
+    }
+
+    public void deletePlaylist(Playlist playlist) {
+        playlists.remove(playlist.getName());
+        System.out.println("Deleted playlist: " + playlist.getName());
+    }
+
+    // retrieve a list all playlists
+    public Map<String, Playlist> getPlaylists() {
+        return playlists;
+    }
+
+    // Method to list all available playlists
+
+    public void listPlaylists() {
+        System.out.println("Available Playlists:");
+        for (String name : playlists.keySet()) {
+            System.out.println("- " + name);
+        }
+    }
+
+
+
+
+
+
+}
+
